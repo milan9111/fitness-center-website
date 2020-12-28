120063545d72951a3b202fb0612fe4a70b9b3d89
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2020-12-28 12:40:41 +0200

    add full site

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.box_trainers_img_slider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:3,
+        speed:1000,
+        centerMode:true,
+        autoplay:true,
+        autoplaySpeed:700,
+        infinite:true,
+    });
+});
+
+
+$(document).ready(function(){
+    $('.box_gallery_img_slider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:3,
+        speed:1000,
+        centerMode:true,
+        autoplay:true,
+        autoplaySpeed:500,
+        infinite:true,
+
+    });
+});
+
+
+$(document).ready(function(){
+    $('.box_reviews_client_slider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        speed:1000,
+    });
+});
+
+    
+    
